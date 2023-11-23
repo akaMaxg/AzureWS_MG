31c3906fe0a9487dbd11eeb57d6a82354d0445db
Max Guclu <m.guclu@live.se>
2023-11-23 18:59:03 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AzureWS_MG.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
